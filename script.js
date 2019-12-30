8eaae72272f795a9a894b2cc39ac96923ce18fff
Benjamin Lincoln <support@lincolnapps.net>
2019-12-30 12:23:49 +1100

    Update script.js

--- script.js
+++ script.js
@@ -48,7 +48,6 @@ function rendertimer() {
     hours = (hours < 10) ? "0" + hours : hours;
     minutes = (minutes < 10) ? "0" + minutes : minutes;
     seconds = (seconds < 10) ? "0" + seconds : seconds;
-
-    //timedisplay.innerHTML =  hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+    
     return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
 }
